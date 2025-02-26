fd565e595f291c97a887c440a8439ac41428102f
Wedad Osama <welkammash@gmail.com>
2025-02-26 04:51:29 +0200

    fix: remove unused reportWebVitals import

--- src/index.js
+++ src/index.js
@@ -2,7 +2,6 @@ import React from "react";
 import ReactDOM from "react-dom/client";
 import "./index.css";
 import App from "./App";
-import reportWebVitals from "./reportWebVitals";
 import { BrowserRouter, Routes, Route, Navigate } from "react-router-dom";
 import SignIn from "./components/pages/SignIn";
 import SignUp from "./components/pages/SignUp";
@@ -21,4 +20,4 @@ root.render(
   </React.StrictMode>
 );
 
-reportWebVitals();
+
